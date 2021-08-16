564780f6fc86b8db62f70b16b9ff3185e36e8286
Dilshan Mudali <35578002+dilshanmudali@users.noreply.github.com>
2021-08-16 04:27:06 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+// Add All
+
+// const addAll = (...nums) => {
+//   // let total = 0;
+//   return nums.reduce((a,b) => a+b,0)
+//   // nums.forEach(num => {
+//   //   total += num
+//   // });
+//   // return total;
+// } 
+
+
+
+// console.log(addAll(2,5,6,7))
